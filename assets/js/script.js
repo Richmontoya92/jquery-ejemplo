4ecda3a9cbe08b1ecf62d1f1cdc53582d614e9c2
Richmontoya92 <r.montoya.alsina@gmail.com>
2025-04-24 16:57:08 +0000

    ejercicio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 console.log('jQuery versión:', $.fn.jquery);
 console.log('Bootstrap Modal existe?', typeof bootstrap.Modal);
 
+$("p").click(function(){
+    $(this).hide(4000)
+    $(this).fadeIn(4000)
+})
 
+$("#boton").click(function(){
+    $("#div").css({
+        'background-color': '#f0ad4e',
+        'color': '#fff',
+        'padding': '10px'
+    })
+})
 
-
+$("#btnToggle").click(function(){
+    $("#panel").slideToggle(150)
+})
